971c10efd311cab38c079258a47696ab94017231
Jacek Sysiak <j4sysiak@gmail.com>
2022-11-27 20:05:54 +0100

    27-11-2022 20:05

---
+++ src/test/java/com/example/demodockerimage/DemoDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
